09601afe9f6f76b9d419e1b91baf0a7d02804b75
Toni Mäkinen <masfonos@gmail.com>
2023-12-09 18:38:20 +0200

    getinput.cmd: fix test input gen

--- bin/getinput.cmd
+++ bin/getinput.cmd
@@ -6,7 +6,7 @@ set DAY=%2
 
 
 rem Session cookie
-set SESSION=
+set SESSION=53616c7465645f5f21a1a45bb671fcb25d229f032d0d5cf3dbf5d3ad662b526986d33138fbca3524011167b010da9a10692f2e81bcb8395eb584eef391f74bab
 
 if "%SESSION%" == "" (
 echo Session key is not set.
@@ -29,11 +29,12 @@ Title AOC %YEAR% Day %DAY% Input
 echo Get AOC %YEAR% Day %DAY% input...
 
 @if not exist "input" mkdir "input"
-curl --silent --cookie "session=%SESSION%" -A "github.com/Masfo/AdventOfCodeTemplate/" %URL%/input -o input\day%day%.txt
+curl --silent --cookie "session=%SESSION%" -A "github.com/Masfo/AdventOfCodeTemplate/" %URL%/input -o input\day%DAY%.txt
 dir /C /N input\day%DAY%.txt | find "day%DAY%.txt"
 
-@echo. 2> input\day%1test.txt
+@echo. 2> input\day%DAY%test.txt
 rem explorer input\day%1test.txt
 
 rem explorer input\day%1.txt
+
 
